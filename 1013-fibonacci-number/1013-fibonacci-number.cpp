bee2c9f54a2689e363938604d1c777a032e0e0a2
Dev Gokha <134613705+DevGokha@users.noreply.github.com>
2025-03-31 01:48:10 +0530

    Time: 0 ms (100%), Space: 7.6 MB (87.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     int fib(int n) {
         
-        if(n==0 || n ==1) return n;
-         int fib1 = fib(n-1);
-         int fib2 = fib(n-2);
-         return fib1+ fib2;
-        
+       if(n==0|| n==1) return n;
+       int a = 0;
+       int b =1;
+       int c= a+b;
+
+       for(int i = 2; i<n; i++){
+        a= b;
+        b=c;
+        c = a+b;
+       }
+        return c;
     }
 };
