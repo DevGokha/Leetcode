6709e1a651abf448fa2471512e10d6491d0fc1fd
Dev Gokha <134613705+DevGokha@users.noreply.github.com>
2025-03-30 16:34:09 +0530

    Time: 5 ms (32.13%), Space: 10 MB (11.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,18 +3,21 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
         unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
         for(int i=0; i<s.length();i++){
             map1[s[i]]++;
         }
         for(int i=0; i<t.length(); i++){
-            char ch = t[i];
-            if(map1.find(ch)!=map1.end()){
-                map1[ch]--;
-                if(map1[ch]==0) map1.erase(ch);
-            }
-            else return false;
+           map2[t[i]]++;
         }
-        if(map1.size()>0) return false;
-        return true;
-    }
+        for(auto x : map1){
+            int ch1 = x.first;
+            int freq1 = x.second;
+            if(map2.find(ch1)!=map2.end()){
+                int freq2 =map2[ch1];
+                if(freq1!=freq2) return false;
+            }else return false;
+
+        } return true;
+        }
 };
