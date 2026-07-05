e263a39821f7ed5e62680cb497c2dddb4f54af8b
Dev Gokha <134613705+DevGokha@users.noreply.github.com>
2026-07-05 23:02:02 +0530

    Time: 23 ms (64.65%), Space: 19.4 MB (100%) - LeetHub

--- 0820-short-encoding-of-words/0820-short-encoding-of-words.py
+++ 0820-short-encoding-of-words/0820-short-encoding-of-words.py
@@ -4,8 +4,4 @@ class Solution:
         for word in words:
             for i in range(1,len(word)):
                 good.discard(word[i:])
-        ans =0
-        
-        for word in good:
-            ans += len(word) + 1
-        return ans
+        return sum(len(word)+1 for word in good)
